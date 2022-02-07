669e6ea905cda845a87128b0dbf4391fd0d43d95
Vladislav <88788337+Justiiiiin@users.noreply.github.com>
2022-02-07 11:40:34 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log(1);
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);  
+console.log(decr--);
+
+console.log(2*4 === 8);
+
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || !isClose);
